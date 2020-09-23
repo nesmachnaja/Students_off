9400a68a01c7ea5b826adf7b93ce8591a5390dbb
Ljudmila <nesmachnaja-ljudmila@rambler.ru>
2020-09-23 23:21:04 +0300

    First commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class FormStud
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отправитьДанныеНаСерверToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(629, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.отправитьДанныеНаСерверToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // отправитьДанныеНаСерверToolStripMenuItem
+            // 
+            this.отправитьДанныеНаСерверToolStripMenuItem.Name = "отправитьДанныеНаСерверToolStripMenuItem";
+            this.отправитьДанныеНаСерверToolStripMenuItem.Size = new System.Drawing.Size(233, 22);
+            this.отправитьДанныеНаСерверToolStripMenuItem.Text = "Отправить данные на сервер";
+            this.отправитьДанныеНаСерверToolStripMenuItem.Click += new System.EventHandler(this.отправитьДанныеНаСерверToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(629, 241);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // FormStud
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 267);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormStud";
+            this.Text = "Студенты";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отправитьДанныеНаСерверToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FormStud : Form
+    {
+        private BindingSource bindingSource = new BindingSource();
+        DataSet dataset = new DataSet();
+
+        public FormStud()
+        {
+            InitializeComponent();
+
+            ReadData();
+        }
+
+        private void кэшированиеДанныхToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReadData();
+        }
+
+        void ReadData()
+        {
+            // соединяемся с сервером
+            SqlConnection connection = new SqlConnection("Data Source=LENOVO-PC\\SQLEXPRESS;Initial Catalog=students;Integrated Security=True;Pooling=false");
+            connection.Open();
+            // подготавливаем команду 
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM students";
+            command.Connection = connection;
+            // создаем адаптер
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            // заполняем набор данных
+            adapter.Fill(dataset);
+            // закрываем соединение, которое нам больше не нужно
+            connection.Close();
+            // связываем набор данных с сеткой через посредника bindingSource
+            dataGridView1.AutoGenerateColumns = true;
+            bindingSource.DataSource = dataset.Tables[0];
+            dataGridView1.DataSource = bindingSource;
+        }
+
+        private void отправитьДанныеНаСерверToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=LENOVO-PC\\SQLEXPRESS;Initial Catalog=students;Integrated Security=True;Pooling=false");
+            connection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM students", connection);
+
+            adapter.UpdateCommand = new SqlCommand("UPDATE students SET Имя = @name, Фамилия = @fam, Группа = @group, Курс = @course, Факультет = @faculty " +
+                "WHERE Id = @id");
+
+            adapter.UpdateCommand.Parameters.Add("@name", SqlDbType.NChar, 10, "Имя");
+            adapter.UpdateCommand.Parameters.Add("@fam", SqlDbType.NChar,10, "Фамилия");
+            adapter.UpdateCommand.Parameters.Add("@group", SqlDbType.NChar, 10, "Группа");
+            adapter.UpdateCommand.Parameters.Add("@course", SqlDbType.NChar, 10, "Курс");
+            adapter.UpdateCommand.Parameters.Add("@faculty", SqlDbType.NChar, 10, "Факультет");
+            adapter.UpdateCommand.Parameters.Add("@id", SqlDbType.Int, 10, "Id");
+
+            adapter.UpdateCommand.Connection = connection;
+
+            adapter.Update(dataset.Tables[0]);
+            connection.Close();
+        }
+    }
+}
